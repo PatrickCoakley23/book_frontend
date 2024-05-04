34fc13aee95471379c330a3f63995843a284e589
Patrick Coakley <patrickcoakley23@gmail.com>
2024-05-04 11:31:18 +0100

    Sorted values from dropdown menu

--- src/container/BookForm/BookForm.jsx
+++ src/container/BookForm/BookForm.jsx
@@ -8,24 +8,21 @@ const BookForm = () => {
         title: "",
         author: "",
         isbn: "",
-        status: "Unread"
+        status: ""
     });
-
-    // const [title, setTitle] = useState([]);
-    // const [author, setAuthor] = useState([]);
-    // const [isbn, setIsbn] = useState([]);
-    // const [status, setStatus] = useState([]);
-
 
     const handleChange = (e) => {
         const value = e.target.value;
+        const name = e.target.name;
         setData({
           ...data,
-          [e.target.name]: value
+          [name]: value
         });
+        
       };
-
-    const handleSubmit = (e) => {
+      console.log(data)
+    
+      const handleSubmit = (e) => {
         //Prevent default refreshing
         e.preventDefault();
         const bookData = {
@@ -47,20 +44,8 @@ const BookForm = () => {
         axios.post("http://127.0.0.1:8000/api/book_add/", bookData, axiosConfig)
         .then((res) => {console.log(res.data, res.status);
         });
-    }
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     const userData = {
-    //       email: data.email,
-    //       password: data.password
-    //     };
-    //     axios.post("https://reqres.in/api/login", userData).then((response) => {
-    //       console.log(response.status, response.data.token);
-    //     });
-    //   };
-
-
+}
+    
 
   return (
     <section className='book-form'>
@@ -68,12 +53,12 @@ const BookForm = () => {
             <div className="form-wrapper py-4 px-4">
                 <div className="row">
                     <div className="col-md-10 offset-md-1">
-                        <form onSubmit={handleSubmit} id="form">
+                        <form id="form" onSubmit={handleSubmit}>
                             <h2 className='text-center mb-4 text-white'>Add To Book List</h2>
                             <input type="title" name="title" value={data.title} onChange={handleChange} className="form-control mb-3" placeholder="Title" />
                             <input type="author" name="author" value={data.author} onChange={handleChange} className="form-control mb-3"placeholder="Author"/>
                             <input type="isbn" name="isbn" value={data.isbn} onChange={handleChange} className="form-control mb-3"placeholder="ISBN"/>
-                            <select className="form-select mb-3" value={data.status} onChange={handleChange}>
+                            <select className="form-select mb-3" name="status" value={data.status}  onChange={handleChange}>
                                 <option >Status</option>
                                 <option value="Unread">Unread</option>
                                 <option value="In Progress">In Progress</option>
